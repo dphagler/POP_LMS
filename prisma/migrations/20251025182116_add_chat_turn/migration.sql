f5a4f07be97b40a1db248c2931bec495c04235fd
hagler <dphagler@gmail.com>
2025-10-25 14:24:05 -0400

    migrations

---
+++ prisma/migrations/20251025182116_add_chat_turn/migration.sql
@@ -0,0 +1,17 @@
+-- DropIndex
+DROP INDEX "public"."Assessment_lessonId_idx";
+
+-- DropIndex
+DROP INDEX "public"."Assessment_userId_idx";
+
+-- DropIndex
+DROP INDEX "public"."AssignmentGroup_assignmentId_idx";
+
+-- DropIndex
+DROP INDEX "public"."AssignmentGroup_groupId_idx";
+
+-- DropIndex
+DROP INDEX "public"."OrgMembership_orgId_idx";
+
+-- DropIndex
+DROP INDEX "public"."OrgMembership_userId_idx";
