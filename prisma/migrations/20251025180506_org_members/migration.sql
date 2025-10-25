725daa81d4b8acf0ab3352dee411c97e8d48f889
hagler <dphagler@gmail.com>
2025-10-25 14:05:25 -0400

    first sql merge

---
+++ prisma/migrations/20251025180506_org_members/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "public"."AssignmentGroup_assignmentId_idx";
+
+-- DropIndex
+DROP INDEX "public"."AssignmentGroup_groupId_idx";
+
+-- DropIndex
+DROP INDEX "public"."OrgMembership_orgId_idx";
+
+-- DropIndex
+DROP INDEX "public"."OrgMembership_userId_idx";
