b27eafb27856c866f381cc83c86b5f611603fe50
Darren Hagler <dphagler@gmail.com>
2025-10-27 10:02:52 -0400

    Expose Sanity env vars for Studio

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,19 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ['i.ytimg.com'] },
+  env: {
+    NEXT_PUBLIC_SANITY_API_VERSION:
+      process.env.NEXT_PUBLIC_SANITY_API_VERSION ??
+      process.env.SANITY_STUDIO_API_VERSION ??
+      '2025-10-21',
+    NEXT_PUBLIC_SANITY_DATASET:
+      process.env.NEXT_PUBLIC_SANITY_DATASET ??
+      process.env.SANITY_STUDIO_DATASET ??
+      process.env.SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_PROJECT_ID:
+      process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ??
+      process.env.SANITY_STUDIO_PROJECT_ID ??
+      process.env.SANITY_PROJECT_ID,
+  },
 };
 export default nextConfig;
