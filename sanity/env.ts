f16f6649f3042ededa4c7b6678cb775e3fb92a71
Darren Hagler <dphagler@gmail.com>
2025-11-02 11:31:43 -0500

    Use browser-safe Sanity env module

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,23 +1,4 @@
-function assertValue<T extends string | undefined>(v: T, msg: string): string {
-  if (!v) throw new Error(`Missing environment variable: ${msg}`)
-  return v
-}
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? 'dkstzfb9'
 
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION ??
-  process.env.SANITY_STUDIO_API_VERSION ??
-  '2025-10-21'
-
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET ??
-    process.env.SANITY_STUDIO_DATASET ??
-    process.env.SANITY_DATASET,
-  'NEXT_PUBLIC_SANITY_DATASET, SANITY_STUDIO_DATASET, or SANITY_DATASET'
-)
-
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ??
-    process.env.SANITY_STUDIO_PROJECT_ID ??
-    process.env.SANITY_PROJECT_ID,
-  'NEXT_PUBLIC_SANITY_PROJECT_ID, SANITY_STUDIO_PROJECT_ID, or SANITY_PROJECT_ID'
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production'
